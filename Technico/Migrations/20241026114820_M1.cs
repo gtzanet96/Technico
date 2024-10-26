7c7b188392f7589bc0fd7fe643baa39024d090cf
Giorgos <gtzanetakis96@gmail.com>
2024-10-26 14:58:01 +0300

    Initialized database and DB schema with EF Core migrations and structure verification

---
+++ Technico/Migrations/20241026114820_M1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Technico.Migrations
+{
+    /// <inheritdoc />
+    public partial class M1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyOwner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VAT = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyOwner", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PropertyOwner_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PropertyIdentificationNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PropertyAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearOfConstruction = table.Column<int>(type: "int", nullable: false),
+                    PropertyType = table.Column<int>(type: "int", nullable: false),
+                    PropertyOwnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PropertyItem_PropertyOwner_PropertyOwnerId",
+                        column: x => x.PropertyOwnerId,
+                        principalTable: "PropertyOwner",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repair",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ScheduledDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(8,2)", precision: 8, scale: 2, nullable: false),
+                    PropertyOwnerId = table.Column<int>(type: "int", nullable: false),
+                    PropertyItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repair", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Repair_PropertyItem_PropertyItemId",
+                        column: x => x.PropertyItemId,
+                        principalTable: "PropertyItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Repair_PropertyOwner_PropertyOwnerId",
+                        column: x => x.PropertyOwnerId,
+                        principalTable: "PropertyOwner",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyItem_PropertyOwnerId",
+                table: "PropertyItem",
+                column: "PropertyOwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyOwner_UserId",
+                table: "PropertyOwner",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repair_PropertyItemId",
+                table: "Repair",
+                column: "PropertyItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repair_PropertyOwnerId",
+                table: "Repair",
+                column: "PropertyOwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Repair");
+
+            migrationBuilder.DropTable(
+                name: "PropertyItem");
+
+            migrationBuilder.DropTable(
+                name: "PropertyOwner");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ Technico/Repositories/TechnicoDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technico.Models;
+
+namespace Technico.Repositories;
+
+public class TechnicoDbContext : DbContext //εδώ ορίζουμε ποια απ τα μοντέλα που έχουμε θα γίνουν πίνακες
+{
+    public DbSet<PropertyOwner> PropertyOwner { get; set; }
+    public DbSet<PropertyItem> PropertyItem { get; set; }
+    public DbSet<Repair> Repair { get; set; }
+    public DbSet<User> User { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Data Source=(local); Initial Catalog=technico-2024; Integrated Security = True; TrustServerCertificate=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Repair>()
+            .HasOne(r => r.PropertyOwner)
+            .WithMany(po => po.Repairs)
+            .HasForeignKey(r => r.PropertyOwnerId)
+            .OnDelete(DeleteBehavior.Restrict); // Prevents cascading delete
+
+        modelBuilder.Entity<Repair>()
+            .HasOne(r => r.PropertyItem)
+            .WithMany(pi => pi.Repairs)
+            .HasForeignKey(r => r.PropertyItemId)
+            .OnDelete(DeleteBehavior.Restrict); // Prevents cascading delete
+    }
+}
