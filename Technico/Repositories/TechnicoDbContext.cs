68b9802c08b5044d7930e39cd6d8d37062390e29
Giorgos <gtzanetakis96@gmail.com>
2024-10-28 11:49:50 +0200

    renamed db tables into plural

--- Technico/Repositories/TechnicoDbContext.cs
+++ Technico/Repositories/TechnicoDbContext.cs
@@ -10,13 +10,25 @@ namespace Technico.Repositories;
 
 public class TechnicoDbContext : DbContext //εδώ ορίζουμε ποια απ τα μοντέλα που έχουμε θα γίνουν πίνακες
 {
-    public DbSet<PropertyOwner> PropertyOwner { get; set; }
-    public DbSet<PropertyItem> PropertyItem { get; set; }
-    public DbSet<Repair> Repair { get; set; }
+    public DbSet<PropertyOwner> PropertyOwners { get; set; }
+    public DbSet<PropertyItem> PropertyItems { get; set; }
+    public DbSet<Repair> Repairs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string connectionString = "Data Source=(local); Initial Catalog=technico-2024; Integrated Security = True; TrustServerCertificate=True;";
         optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<PropertyOwner>()
+            .HasIndex(o => o.VAT)
+            .IsUnique();
+
+        modelBuilder.Entity<PropertyOwner>().ToTable("PropertyOwners");
+        modelBuilder.Entity<PropertyItem>().ToTable("PropertyItems");
+        modelBuilder.Entity<Repair>().ToTable("Repairs");
     }
 }
