1c437c42a39d331d670200e6593464381eb103ff
Giorgos <gtzanetakis96@gmail.com>
2024-10-29 23:32:18 +0200

    adding recent migrations

---
+++ Technico/Migrations/20241029212944_M4.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Technico.Migrations
+{
+    /// <inheritdoc />
+    public partial class M4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyIdentificationNumber",
+                table: "PropertyItems",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyItems_PropertyIdentificationNumber",
+                table: "PropertyItems",
+                column: "PropertyIdentificationNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PropertyItems_PropertyIdentificationNumber",
+                table: "PropertyItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyIdentificationNumber",
+                table: "PropertyItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
