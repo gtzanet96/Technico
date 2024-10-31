f8ac9896e1a3a3a8fd911d9a5b29cb762be56b07
Giorgos <gtzanetakis96@gmail.com>
2024-10-31 22:26:46 +0200

    - final data for program.cs testing scenarios

--- Technico/Program.cs
+++ Technico/Program.cs
@@ -10,15 +10,13 @@ Console.WriteLine("Hello, Mr. Iracleous!");
 TechnicoDbContext db = new TechnicoDbContext();
 
 /* για τον Owner: */
-var owner1 = new PropertyOwner()
+PropertyOwner[] owners = new PropertyOwner[]
 {
-    FirstName = "Albert",
-    LastName = "Ca",
-    PhoneNumber = "210232367890",
-    VAT = "1777",
-    Address = "L K 17-19",
-    Email = "astranger@gmail.com",
-    Password = "Password123"
+    new PropertyOwner { FirstName = "Albert", LastName = "Ca", PhoneNumber = "210232367890", VAT = "11111", Address = "L K 17-19", Email = "albert1@gmail.com", Password = "Password123" },
+    new PropertyOwner { FirstName = "Berta", LastName = "Mo", PhoneNumber = "210232367891", VAT = "22222", Address = "M L 18-20", Email = "berta1@gmail.com", Password = "Password123" },
+    new PropertyOwner { FirstName = "Charlie", LastName = "Do", PhoneNumber = "210232367892", VAT = "33333", Address = "N K 19-21", Email = "charlie1@gmail.com", Password = "Password123" },
+    new PropertyOwner { FirstName = "Dora", LastName = "Pa", PhoneNumber = "210232367893", VAT = "44444", Address = "O L 20-22", Email = "dora1@gmail.com", Password = "Password123" },
+    new PropertyOwner { FirstName = "Edward", LastName = "Ra", PhoneNumber = "210232367894", VAT = "55555", Address = "P K 21-23", Email = "edward1@gmail.com", Password = "Password123" }
 };
 
 PropertyOwnerService ownerService = new PropertyOwnerService(db);
@@ -27,11 +25,14 @@ PropertyOwnerService ownerService = new PropertyOwnerService(db);
 //Σημείωση: Σε περίπτωση που αφήσετε και κενό κάποιο πεδίο και ίδιο το vat με κάποιο προηγούμενο vat, το vat error χτυπάει πριν από το null field error,
 //παρότι το validation του είναι δεύτερο σε σειρά στο service create method! Από ότι καταλαβαίνω αυτό συμβαίνει γιατί έχοντας βάλει unique constraint
 //για το VAT στη βάση, γίνεται trigger σε πρώτη προτεραιότητα το database-level validation error.
-//PropertyCustomResponse response = ownerService.CreatePropertyOwner(owner1);
-//PropertyCustomResponse response = ownerService.UpdatePropertyOwner(2, owner1);
-//PropertyCustomResponse response = ownerService.DeletePropertyOwner(3);
+foreach (var owner in owners)
+{
+    PropertyCustomResponse response = ownerService.CreatePropertyOwner(owner); //parameters: (new owner)
+    Console.WriteLine(response.Message);
+}
+//PropertyCustomResponse response = ownerService.UpdatePropertyOwner(2, owner1); //parameters: (owner id, new owner)
+//PropertyCustomResponse response = ownerService.DeletePropertyOwner(3); //parameters: (owner id)
 //Console.WriteLine(response.Message);
-//2)
 
 // ***** Get owner personal details *****
 /*var owner = ownerService.GetPropertyOwnerDetails(2); //parameters: (owner id)
@@ -105,15 +106,23 @@ else
 /* για το Service: */
 PropertyItemService itemService = new PropertyItemService(db);
 
-var item1 = new PropertyItem()
+PropertyItem[] items = new PropertyItem[]
 {
-    PropertyIdentificationNumber = 1209887723,
-    PropertyAddress = "hj K 11",
-    YearOfConstruction = 1990,
-    PropertyType = PropertyType.DetachedHouse
+    new PropertyItem { PropertyIdentificationNumber = 1209887723, PropertyAddress = "Hj K 11", YearOfConstruction = 1990, PropertyType = PropertyType.DetachedHouse },
+    new PropertyItem { PropertyIdentificationNumber = 1209887724, PropertyAddress = "Kj L 12", YearOfConstruction = 1985, PropertyType = PropertyType.ApartmentBuilding },
+    new PropertyItem { PropertyIdentificationNumber = 1209887725, PropertyAddress = "Lj M 13", YearOfConstruction = 2000, PropertyType = PropertyType.Maisonet },
+    new PropertyItem { PropertyIdentificationNumber = 1209887726, PropertyAddress = "Mj N 14", YearOfConstruction = 1995, PropertyType = PropertyType.DetachedHouse },
+    new PropertyItem { PropertyIdentificationNumber = 1209887727, PropertyAddress = "Nj O 15", YearOfConstruction = 2010, PropertyType = PropertyType.ApartmentBuilding }
 };
 // ***** Create-Update-Add & Remove Coowner-Delete ***** 
-//PropertyCustomResponse response = itemService.CreatePropertyItem(item1, "1010"); //parameters: (new item, owner vat)
+
+// ***** Create-Update-Add & Remove Coowner-Delete ***** 
+foreach (var item in items)
+{
+    PropertyCustomResponse response = itemService.CreatePropertyItem(item, "22222"); //parameters: (new item, owner vat) - δημιουργεί 5 property items για τον ίδιο owner!
+    Console.WriteLine(response.Message);
+}
+//PropertyCustomResponse response = itemService.CreatePropertyItem(item1, "22222"); //parameters: (new item, owner vat) - δημιουργεί 1 property item για 1 owner
 //PropertyCustomResponse response = itemService.UpdatePropertyItem(3, item1); //parameters: (item id, new item)
 //PropertyCustomResponse response = itemService.AddCoOwnerToPropertyItem(1, "777"); //parameters: (property id, owner vat)
 //PropertyCustomResponse response = itemService.RemoveCoOwnerFromPropertyItem(1, "777");parameters:  //(property id, owner vat)
@@ -141,18 +150,21 @@ var item1 = new PropertyItem()
 
 /* για το Repair: */
 PropertyRepairService repairService = new PropertyRepairService(db);
-
-var repair1 = new PropertyRepair()
+PropertyRepair[] repairs = new PropertyRepair[]
 {
-    ScheduledDate = new DateTime(2025, 12, 21, 15, 30, 00),
-    RepairType = RepairType.ElectricalWork,
-    RepairDescription = "good house",
-    Cost = 10M,
-    RepairStatus = RepairStatus.InProgress
+    new PropertyRepair { ScheduledDate = new DateTime(2025, 12, 21, 15, 30, 00), RepairType = RepairType.ElectricalWork, RepairDescription = "good house", Cost = 10M, RepairStatus = RepairStatus.InProgress },
+    new PropertyRepair { ScheduledDate = new DateTime(2025, 11, 15, 10, 45, 00), RepairType = RepairType.Plumbing, RepairDescription = "pipe repair", Cost = 15M, RepairStatus = RepairStatus.Pending },
+    new PropertyRepair { ScheduledDate = new DateTime(2025, 10, 10, 9, 30, 00), RepairType = RepairType.Painting, RepairDescription = "wall repaint", Cost = 20M, RepairStatus = RepairStatus.Complete },
+    new PropertyRepair { ScheduledDate = new DateTime(2025, 9, 5, 14, 00, 00), RepairType = RepairType.Insulation, RepairDescription = "roof insulation", Cost = 25M, RepairStatus = RepairStatus.Pending },
+    new PropertyRepair { ScheduledDate = new DateTime(2025, 8, 1, 8, 15, 00), RepairType = RepairType.Frames, RepairDescription = "window frames", Cost = 18M, RepairStatus = RepairStatus.InProgress }
 };
-
-// ***** Create-Update-Delete ***** 
-//PropertyCustomResponse response = repairService.CreateRepair(repair1, 4); //parameters: (new repair, property item id)
+// ***** Create-Update-Delete *****
+foreach (var repair in repairs)
+{
+    PropertyCustomResponse response = repairService.CreateRepair(repair, 1); //parameters: (new repair, property item id) - δημιουργεί 5 repairs για το ίδιο property item!
+    Console.WriteLine(response.Message);
+}
+//PropertyCustomResponse response = repairService.CreateRepair(repair1, 4); //parameters: (new repair, property item id) - δημιουργεί 1 repair για 1 property item!
 //PropertyCustomResponse response = repairService.UpdateRepair(1, repair1); //parameters: (repair id, new repair)
 //PropertyCustomResponse response = repairService.DeleteRepair(2, false); //parameters: (repair id, false=hard delete και true=soft delete)
 //Console.WriteLine(response.Message);
